10d54c95f73b6ecd7c8661dd0d026d0d87180899
redmblue <47989545+redmblue@users.noreply.github.com>
2021-01-02 21:49:41 -0500

    Initial commit

---
+++ src/main/java/mainpack/Main.java
@@ -0,0 +1,11 @@
+package mainpack;
+//import org.tensorflow.;
+public class Main {
+    public static void main(String args[]){
+        //step 1: find occurence in csv. Step 2: Get weather at the nearest time.
+        // Step 3: Analyze using current model - add percent of occurance happening in there
+        //Step 4: make model and add percent of occurance happening - this file
+
+
+    }
+}
